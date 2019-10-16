6b36b69c5e813ae98b1eabcdc797ab91698ca168
Wilson Neto <wilson.neto@encripta.com.br>
2019-10-16 14:20:13 -0300

    fix: pagina de sugestões erro http/https

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -52,7 +52,7 @@ export const servicesAPIs = {
     }
   },
 
-  suggestions: 'http://demo1589835.mockable.io/suggestions',
+  suggestions: 'https://demo1589835.mockable.io/suggestions',
 
   channelVideos:
     'https://www.googleapis.com/youtube/v3/search?key=AIzaSyDokzfhBGfUkE_ezOsRlZdR0nOVuWRYYhc&channelId=UCl8MCsaxzWnm_D4izigpTeg&part=snippet,id&order=date&maxResults=3',
