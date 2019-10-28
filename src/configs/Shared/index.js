513264952d3f2371d73072459c6bb91d97f6b405
Wilson Neto <wilson.neto@encripta.com.br>
2019-10-28 11:20:29 -0300

    alterando os conteúdos para os conteúdos da belas artes

--- src/configs/Shared/index.js
+++ src/configs/Shared/index.js
@@ -1,13 +1,13 @@
 const Shared = {
   defaultTitle: 'Petra Belas Artes',
 
-  /*
   sectionLocationId: 9033,
   AuthenticationTicket: 'looke@looke:v7c8ad@#$',
-  */
 
+  /*
   sectionLocationId: 8001,
   AuthenticationTicket: 'univer@univer:e4rt5@#r$',
+  */
 
   BannerAuthenticationTicket: 'looke@looke:v7c8ad@#$',
   BannerLocationId: 907
