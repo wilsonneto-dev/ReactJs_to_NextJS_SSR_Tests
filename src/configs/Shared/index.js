5ca0e818d9615b5f085fdeb1acbab320667bdb10
Wilson Neto <wilson.neto@encripta.com.br>
2019-11-01 13:49:02 -0300

    testing ie 11

--- src/configs/Shared/index.js
+++ src/configs/Shared/index.js
@@ -7,7 +7,7 @@ const Shared = {
   BannerLocationId: 907,
   BannerAuthenticationTicket: 'looke@looke:v7c8ad@#$',
 
-  platformHost: 'https://www.looke.com.br'
+  platformHost: 'http://looke.com.br'
 };
 
 export default Shared;
